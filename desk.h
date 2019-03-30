59dd766235bdc855a4e496e00076b5e1a97b119d
amagood <46104212+amagood@users.noreply.github.com>
2019-03-30 21:05:58 +0800

    Add files via upload

--- desk.h
+++ desk.h
@@ -5,6 +5,8 @@ class Desk
 {
 private:
     int hp0,hp1;
+    bool isEnd;
+    int winLose; //0->p0 wins 1-> p1 wins
     ///declare:
     /**
         p0 deck
@@ -14,13 +16,14 @@ private:
         p1 hand
     */
 public:
-    Desk();
+    Desk(Deck deck0, Deck deck1, String name0, String name1);
     ~Desk();
 
     Card draw(int num);
 
     void playerMovement(int playerId);
 
-
+    bool isEnd();
+    int getWinPlayer();
 };
 #endif // AMAGOOD_DESK_CPP
