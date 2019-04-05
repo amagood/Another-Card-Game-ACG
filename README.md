1a78ed74b29c245897f1224420a058dd77c16ff7
Jacky Ho <jackyhobingo@gmail.com>
2019-04-06 04:29:40 +0800

    Enable gtest framework for testing ACG.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Another-Card-Game-ACG
+
+Test code
+-
+If you want to test code, you might add gtest framework to directory lib.
+The path is "/lib/googletest-master"
---
+++ test_main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by jacky on 2019/4/6.
+//
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+int main(int argc, char* argv[]){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
