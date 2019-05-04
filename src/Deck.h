6f1038791d2b223a00ef6cfb536124c363de74f2
vbscript055246 <30942066+vbscript055246@users.noreply.github.com>
2019-05-04 22:41:14 +0800

    Add files via upload

--- src/Deck.h
+++ src/Deck.h
@@ -24,7 +24,7 @@ public:
     Card popDeck(int index);
     //-1 抽一張牌, others just pop by index
 
-    Card &getIndexCards(int mode, int sieve);
+    Card &getIndexCards(int num, int mode = 0, int sieve = -1);
     //取得Card reference, mode=0 (random抽取), mode=1 (從牌堆最前面), mode=-1 (從牌堆最後面), sieve=-1 所有牌,others 只有那種type的牌
 
     int size();
