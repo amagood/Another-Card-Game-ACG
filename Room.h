0c86f2bd72fb1540a1814f724ecf857f56bd43c0
WenRong1213108 <48859765+WenRong1213108@users.noreply.github.com>
2019-05-10 23:54:21 +0800

    Update Room.h
    json, thread

--- Room.h
+++ Room.h
@@ -3,6 +3,10 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include "AccountSystem.h"
+#include "nlohmann/json.hpp"
+#include "Reader.h"
+#include "Printer.h"
 
 
 #ifndef PLAYER
@@ -10,12 +14,12 @@
 class Player
 {
 public:
-    Player(int id, std::string name, Deck & deck) : _ID(id), _name(name), _deck(deck){}
-    int getID() const { return _ID; }
+    Player(uint32_t id, std::string name, Deck & deck) : _ID(id), _name(name), _deck(deck){}
+    uint32_t getID() const { return _ID; }
     std::string getName() const { return _name; }
     Deck & getDeck() const { return _deck; }
 private:
-    int _ID;
+    uint32_t _ID;
     std::string _name;
     Deck & _deck;
 };
@@ -29,24 +33,27 @@ class Room
 public:
     Room(int id, std::string name, std::string word, Player* player);
     ~Room();
+    static Room* createRoom(Player* player, RoomMode mode, int id, std::string name, std::string password);
     int getID() const { return _ID; }
     std::string getName() const { return _name; }
     std::vector<Player*> getPlayers() const { return _player; }
     RoomMode getRoomMode() const { return _mode; }
     bool isPasswordCorrect(std::string word) const { return _password=="" || _password==word; }
-    void getRoomInfo() const {}
+    bool isGameEnd() const { return _endgame; }
     virtual bool isFull() const =0;
     virtual bool addPlayer(Player* player)=0;
 	virtual void startGame()=0;
+	virtual void endGame()=0;
 	virtual bool invitePlayer(int id)=0;
+    virtual uint32_t getWinnerID()=0;
+    virtual uint32_t getLoserID()=0;
 protected:
     int _ID;
     std::string _name;
     std::string _password;
     std::vector<Player*> _player;
     RoomMode _mode;
-    Player* getPlayerInfo(int id);
-	virtual void sendGameResult()=0;
+    bool _endgame=false;
 };
 class OneOnOneRoom : public Room
 {
@@ -56,9 +63,12 @@ public:
     bool addPlayer(Player* player) override;
     bool invitePlayer(int id) override;
 	void startGame() override;
+	void endGame() override;
+	uint32_t getWinnerID() override;
+	uint32_t getLoserID() override;
 private:
+    Desk* _desk;
     static constexpr short MaxPlayerNum=2;
-	void sendGameResult() override;
 };
 class LadderRoom : public Room
 {
@@ -68,9 +78,11 @@ public:
     bool addPlayer(Player* player) override;
     bool invitePlayer(int id) override;
 	void startGame() override;
+	void endGame() override;
+	uint32_t getWinnerID() override;
+	uint32_t getLoserID() override;
 private:
     static constexpr short MaxPlayerNum=2;
-	void sendGameResult() override;
 };
 #endif // ROOM
 
@@ -80,23 +92,42 @@ class Arena
 {
     friend std::ostream & operator << (std::ostream & os, const Arena* arena);
 public:
-    Arena();
+    Arena(Reader *in, Sender *out, AccountSystem *acc);
     ~Arena();
-    ///«Ø¥ß©Ð¶¡¡ATrue:«Ø¥ß¦¨¥\¡AFalse:¦WºÙ­«½Æ
-    bool createRoom(int playerID, std::string playerName, Deck & playerDeck, RoomMode mode, std::string name, std::string password);
-    bool enterRoom(int playerID, std::string playerName, Deck & playerDeck, RoomMode mode, int id, std::string password);
-    void enterRoomRandom(int playerID, std::string playerName, Deck & playerDeck, RoomMode mode);
+    ///å»ºç«‹æˆ¿é–“ï¼Œå»ºç«‹æˆåŠŸå›žå‚³idï¼Œ-1:Failedï¼Œåç¨±é‡è¤‡
+    int createRoom(uint32_t playerID, RoomMode mode, std::string name, std::string password);
+    bool enterRoom(uint32_t playerID, RoomMode mode, int id, std::string password);
+    void enterRoomRandom(uint32_t playerID, RoomMode mode);
+    void inviteFriend(uint32_t playerID, RoomMode mode, int id);
+    void readJson();
 private:
+    Reader *reader;
+    Sender *sender;
+    AccountSystem* account;
     std::vector<Room*> _room[ROOMMODE_COUNT];
-    const std::string myJsonType="room";
+    bool running=true;
+    const std::string jsonType="room";
+    const int jsonID=123;
     void initArena();
     int getNonRepeatRandomRoomID(RoomMode mode);
     std::string getRandomString(RoomMode mode);
     std::string getNonRepeatRandomRoomName(RoomMode mode);
     void createRoom(Player* player, RoomMode mode, int id, std::string name, std::string password);
     Room* getRoom(RoomMode mode, int id);
-    void freeAllRooms();
     bool isRoomNameAdmitted(std::string name, RoomMode mode);
+    Player* createPlayer(uint32_t playerID);
+    void checkRooms();
+    void freeAllRooms();
+    ///UI
+    enum ActionType { GET_ROOMLIST=0, GET_ROOMINFO, CREATE_ROOM, ENTER_ROOM, ENTER_ROOM_RANDOM, INVITE_FRIEND, ACTION_COUNT};
+    static const std::string actionString[];
+    void setJson(json& j);
+    int getActionType(std::string action);
+    void returnRoomList(RoomMode mode);
+    void returnRoomInfo(RoomMode mode, int roomId);
+    void returnCreateRoom(uint32_t playerID, RoomMode mode, std::string name, std::string password);
+    void returnEnterRoom(uint32_t playerID, RoomMode mode, int id, std::string password);
 };
+const std::string Arena::actionString[]={"getRoomList", "getRoomInfo", "createRoom", "enterRoom", "enterRoomRandom", "inviteFriend"}
 #endif // ARENA
 
