b59801b2f7673579fe788d83497198d428c56db0
Shang Chi Wu <44619143+shangchiwu@users.noreply.github.com>
2019-05-25 20:13:39 +0800

    Reconstruct to object oriented style

--- game-server/game-server.js
+++ game-server/game-server.js
@@ -1,126 +1,41 @@
 const readline = require('readline');
-const {spawn} = require('child_process');
-//const wingston = require('winston');
-const printf = require('printf');
 const socketIo = require('socket.io');
+const {spawn} = require('child_process');
+const printf = require('printf');
+//const wingston = require('winston');
+const colors = require('colors');
+
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'cyan',
+  error: 'red'
+});
 
 /*
-  global variables
-*/
+ * global variables
+ */
 
-let CONFIG = require('./server-config.json');
+const configFilePath = './server-config.json';
+let CONFIG = require(configFilePath);
+let logger = null;
+
 let clientSide = null;
 let gameLogicEngine = null;
 
-/* 
-  init command line
-*/
-
-// config command line mode
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: 'ACG>',
-  completer: (input) => {
-    const keyWords = Object.keys(commands).sort();
-    const hits = keyWords.filter((key) => key.startsWith(input));
-    return [hits.length ? hits : keyWords, input];
-  }
-});
-
-// commands and their handler
-const commands = {
-  'help': {
-    alias: 'h',
-    description: 'List all commands and their descriptions.',
-    handler: commandHelp
-  },
-
-  'quit': {
-    alias: 'q',
-    description: 'Terminate all servers and exit this command line.',
-    handler: commandQuit
-  },
-
-  'cls': {
-    description: 'Clear screen.',
-    handler: commandCls
-  },
-
-  'loadConfig': {
-    alias: 'lc',
-    description: 'Reload config file.',
-    handler: commandLoadConfig
-  },
-
-  'initClientSide': {
-    alias: 'ic',
-    description: 'Init the Client Side server.',
-    handler: commandInitClientSide
-  },
-
-  'stopClientSide': {
-    alias: 'sc',
-    description: 'Stop the Client Side server.',
-    handler: commandStopClientSide
-  },
-
-  'initGameLogicEngine': {
-    alias: 'ig',
-    description: 'Run Game Logic Engine in new child process.',
-    handler: commandInitGameLogicEngine
-  },
-
-  'stopGameLogicEngine': {
-    alias: 'sg',
-    description: 'Stop the Game Logic Engine.',
-    handler: commandStopGameLogicEngine
-  },
-
-  'sendMessage': {
-    alias: 'm',
-    description: 'Send message to given target: [g]: Game Logic Engine',
-    handler: commandSendMessage
-  }
-}
-
-// initial text
-console.log('+-------------------------------+\n' + 
-            '|        ACG Game Server        |\n' +
-            '+-------------------------------+\n' +
-            'type \'help\' to list all commands.');
-rl.prompt(true);
-
-// listen to command line
-rl.on('line', (input) => {
-  input = input.trim();
-  const command = input.split(' ')[0];
-
-  let aliases = {};
-  for (const fullName in commands) {
-    if (commands[fullName].alias)
-      aliases[commands[fullName].alias] = fullName;
-  }
-
-  if (commands[command]) {
-    commands[command].handler(input, rl);
-  } else if (aliases[command]) {
-    commands[aliases[command]].handler(input, rl);
-  } else if(input.length === 0) {
-    // do nothing
-  } else {
-    console.log(`unknow command: \'${input}\'`);
-  }
-
-  rl.prompt(true);
-});
-
 /*
-  command line functinos
-*/
+ * command line functinos
+ */
 
 // exit this console
 function commandQuit(input, rl) {
+  logger.info(`Quit Game server...`)
   rl.close();
   process.exit();
 }
@@ -133,44 +48,45 @@ function commandCls() {
 
 // show command line help
 function commandHelp() {
-  const formatString = '  %-20s  %-5s  %s';
-  console.log(printf(formatString, 'command', 'alias', 'description'));
-  console.log('------------------------------------------------------------');
+  const commands = gameServer.commandLine.commands;
+  const formatString = `  ${`%-20s`.bold}  ${`%-5s`.yellow.bold}  %s`;
+  logger.log(printf(formatString, 'command', 'alias', 'description').bold);
+  logger.log('------------------------------------------------------------');
 
   Object.keys(commands).sort().forEach((command) => {
-    console.log(printf(formatString,
-                       command,
-                       commands[command].alias || '',
-                       commands[command].description));
+    logger.log(printf(formatString,
+                      command,
+                      commands[command].alias || '',
+                      commands[command].description));
   });
 }
 
 // load config file from disk
 function commandLoadConfig() {
-  CONFIG = require('./server-config.json');
-  console.log('config loadded from \'./server-config.json\'');
+  CONFIG = require(configFilePath);
+  logger.log(`config loadded from '${configFilePath}'`);
 }
 
-// init socket.io server
+// init client side server
 function commandInitClientSide() {
-  console.log('init Client Side server...');
+  logger.log(`init Client Side server...`);
 
   if (clientSide) {
-    console.error('Client Side server is already running.');
+    logger.error(`Client Side server is already running.`);
     return;
   }
 
-  clientSide = new ClientSide();
+  clientSide = new ClientSide({port: CONFIG.socket_server_port});
 }
 
-// stop socket.io server
+// stop client side server
 function commandStopClientSide() {
   if (!clientSide) {
-    console.error('Client Side server isn\'t running.');
+    logger.error(`Client Side server isn't running.`);
     return;
   }
 
-  console.log('Stopping Client Side server...');
+  logger.log(`Stopping Client Side server...`);
 
   clientSide.stop(() => {
     clientSide = null;
@@ -180,22 +96,22 @@ function commandStopClientSide() {
 // init Game Logic Engine
 function commandInitGameLogicEngine() {
   if (gameLogicEngine) {
-    console.error(`Game Logic Engine is already running with pid ${gameLogicEngine.getPid()}.`);
+    logger.error(`Game Logic Engine is already running with pid ${gameLogicEngine.getPid()}.`);
     return;
   }
 
-  console.log('init Game Logic Engine.');
+  logger.log(`init Game Logic Engine...`);
   gameLogicEngine = new GameLogicEngine();
 }
 
 // stop the Game Logic Engine
 function commandStopGameLogicEngine() {
   if (!gameLogicEngine) {
-    console.error('Game Logic Engine isn\'t running.');
+    logger.error(`Game Logic Engine isn't running.`);
     return;
   }
 
-  console.log('Stopping Game Logic Engine...');
+  logger.log(`Stopping Game Logic Engine...`);
   gameLogicEngine.stop(() => {
     gameLogicEngine = null;
   });
@@ -210,16 +126,158 @@ function commandSendMessage(input) {
     if (gameLogicEngine) {
       gameLogicEngine.send(message);
     } else {
-      console.error('Game Logic Engine isn\'t running.');
+      console.error(`Game Logic Engine isn't running.`);
+    }
+  } else if (target === 'c') {
+    if (clientSide) {
+      const id = message.split(' ')[0];
+      const data = message.substr(message.indexOf(' ') + 1);
+      clientSide.send(id, data);
+    } else {
+      console.error(`Client Side Server isn't running.`);
     }
   } else {
-    console.error('unknow target.');
+    console.error(`unknow target.`);
   }
 }
 
 /*
-  utility classes and functions
-*/
+ * utility classes and functions
+ */
+
+class GameServer {
+  constructor() {
+    this.commandLine = new CommandLine();
+  }
+}
+
+class Logger {
+  constructor(options = {}) {
+    this.console = options.console || console;
+  }
+
+  log(str) {
+    this.console.log(str.toString());
+  }
+
+  error(str) {
+    this.console.error('ERROR'.bgRed.bold + ' ' + str.toString().error.bold);
+  }
+
+  info(str) {
+    this.console.info('INFO'.bgGreen.bold + ' ' + str.toString().info.bold);
+  }
+
+  debug(str) {
+    this.console.debug('DEBUG'.bgCyan.bold + ' ' + str.toString().debug.bold);
+  }
+}
+
+class CommandLine {
+  constructor(options) {
+    
+    // commands and their handler
+    this.commands = {
+      'help': {
+        alias: 'h',
+        description: 'List all commands and their descriptions.',
+        handler: commandHelp
+      },
+
+      'quit': {
+        alias: 'q',
+        description: 'Terminate all servers and exit this command line.',
+        handler: commandQuit
+      },
+
+      'cls': {
+        description: 'Clear screen.',
+        handler: commandCls
+      },
+
+      'loadConfig': {
+        alias: 'lc',
+        description: 'Reload config file.',
+        handler: commandLoadConfig
+      },
+
+      'initClientSide': {
+        alias: 'ic',
+        description: 'Init the Client Side server.',
+        handler: commandInitClientSide
+      },
+
+      'stopClientSide': {
+        alias: 'sc',
+        description: 'Stop the Client Side server.',
+        handler: commandStopClientSide
+      },
+
+      'initGameLogicEngine': {
+        alias: 'ig',
+        description: 'Run Game Logic Engine in new child process.',
+        handler: commandInitGameLogicEngine
+      },
+
+      'stopGameLogicEngine': {
+        alias: 'sg',
+        description: 'Stop the Game Logic Engine.',
+        handler: commandStopGameLogicEngine
+      },
+
+      'sendMessage': {
+        alias: 'm',
+        description: 'Send message to: [g] Game Logic Engine, [c] Client Side',
+        handler: commandSendMessage
+      }
+    };
+
+    // config command line mode
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: 'ACG>'.gray,
+      completer: (input) => {
+        const keyWords = Object.keys(this.commands).sort();
+        const hits = keyWords.filter((key) => key.startsWith(input));
+        return [hits.length ? hits : keyWords, input];
+      }
+    });
+    
+    // initial text
+    logger.log((`+-------------------------------+\n` + 
+                `|        ACG Game Server        |\n` +
+                `+-------------------------------+\n`).rainbow.bgWhite +
+                `type '${`help`.yellow.bold}' to list all commands.`);
+    this.rl.prompt(true);
+
+    // listen to command line
+    this.rl.on('line', (input) => {
+      input = input.trim();
+      const command = input.split(' ')[0];
+
+      // make alias
+      let aliases = {};
+      for (const fullName in this.commands) {
+        if (this.commands[fullName].alias)
+          aliases[this.commands[fullName].alias] = fullName;
+      }
+
+      // try to match command
+      if (this.commands[command]) {
+        this.commands[command].handler(input, this.rl);
+      } else if (aliases[command]) {
+        this.commands[aliases[command]].handler(input, this.rl);
+      } else if(input.length === 0) {
+        // press enter, do nothing
+      } else {
+        logger.error(`unknow command: '${input}'`);
+      }
+
+      this.rl.prompt(true);
+    });
+  }
+}
 
 class GameLogicEngine {
   constructor() {
@@ -239,13 +297,13 @@ class GameLogicEngine {
 
     // listen to the events of the process
     this.process.on('close', () => {
-      console.log(`[GameLogicEngine]: server is stop.`);
+      logger.info(`[GameLogicEngine]: server is stop.`);
       if (this._handlers.close)
         this._handlers.close();
     });
 
     this.process.on('error', () => {
-      console.error('[GameLogicEngine]: some error occured.');
+      logger.error(`[GameLogicEngine]: some error occured.`);
     });
 
     this.process.stdout.on('data', (data) => {
@@ -258,7 +316,7 @@ class GameLogicEngine {
       (this._handlers.stderr || this._defaultOnStderrHandler)(data);
     });
     
-    console.log(`[GameLogicEngine]: server runs with pid ${this.getPid()}.`);
+    logger.info(`[GameLogicEngine]: server runs with pid ${this.getPid()}.`);
   }
 
   getPid() {
@@ -279,18 +337,18 @@ class GameLogicEngine {
   }
 
   _defaultOnDataHandler(data) {
-    console.log(`[GameLogicEngine]: data >>>${data}<<<`);
+    logger.log(`[GameLogicEngine]: data:\n>>>${data}<<<`);
   }
 
   _defaultOnStderrHandler(data) {
-    console.log(`[GameLogicEngine]: stderr >>>${data}<<<`);
+    logger.log(`[GameLogicEngine]: stderr:\n>>>${data}<<<`);
   }
 }
 
 class ClientSide {
   constructor(options) {
     // properties
-    this.socketIoPort = CONFIG.socket_server_port;
+    this.socketIoPort = options.port;
     this.ioServer = null;
 
     // create socket.io server
@@ -299,22 +357,38 @@ class ClientSide {
 
     // listen to events of the socket.io server
     io.sockets.on('connection', (client) => {
-      console.log(`[ClientSide]: client connected.`);
+      logger.debug(`[ClientSide]: client ${client.id} connected.`);
 
-      client.on('sendData', (data) => {
-        console.log(`[ClientSide]: data from '${client.id}': >>>'${JSON.stringify(data, null, 2)}'<<<`);
-        client.broadcast.to(client.id).emit('sendData', data);
+      client.on('dataToServer', (data) => {
+        logger.debug(`[ClientSide]: data from '${client.id}':\n>>>${JSON.stringify(data, null, 2)}<<<`);
+        //this.broadcast(data);
+        this.send(client.id, data);
       });
     });
 
-    console.log(`[ClientSide]: server runs at port ${CONFIG.socket_server_port}.`);
+    logger.info(`[ClientSide]: server runs at port ${this.socketIoPort}.`);
   }
 
   stop(callback) {
     this.ioServer.close(() => {
       this.ioServer = null;
-      console.log('[ClientSide]: server is stop.');
+      logger.info(`[ClientSide]: server is stop.`);
       callback();
     });
   }
+
+  send(id, data) {
+    this.ioServer.to(id).emit('dataToClient', data);
+  }
+
+  broadcast(data) {
+    this.ioServer.emit('dataToClient', data);
+  }
 }
+
+/*
+ * Run Server
+ */
+
+logger = new Logger();
+const gameServer = new GameServer();
