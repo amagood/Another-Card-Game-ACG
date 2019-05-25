b9f4149b3c16c0f81f7db6f3e354842b39d9db51
Jacky Ho <jackyhobingo@gmail.com>
2019-05-26 02:49:53 +0800

    Add ArenaController

---
+++ src/ArenaController.cpp
@@ -0,0 +1,7 @@
+//
+// Created by jacky on 2019/5/26.
+//
+
+#include "ArenaController.h"
+
+// TODO Build ArenaController
---
+++ src/ArenaController.h
@@ -0,0 +1,16 @@
+//
+// Created by jacky on 2019/5/26.
+//
+
+#ifndef ANOTHER_CARD_GAME_ACG_ARENACONTROLLER_H
+#define ANOTHER_CARD_GAME_ACG_ARENACONTROLLER_H
+
+#include <nlohmann/json.hpp>
+class AccountSystem;
+class ArenaController {
+    ArenaController();
+    nlohmann::json run(nlohmann::json &);
+};
+
+
+#endif //ANOTHER_CARD_GAME_ACG_ARENACONTROLLER_H
