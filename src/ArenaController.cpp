3f39c8e8a7f9dc8dbd44a4067dfd28b6bc9131d0
Violet <sandia1213108@gmail.com>
2019-06-01 21:09:28 +0800

    ArenaController return json

--- src/ArenaController.cpp
+++ src/ArenaController.cpp
@@ -7,44 +7,57 @@
 #include "Arena.h"
 
 
-ArenaController::ArenaController(AccountSystem *acc) : account(acc) , arena(Arena(acc))
-{
-
-}
 nlohmann::json ArenaController::run(nlohmann::json json)
 {
+    nlohmann::json data;
     nlohmann::json result;
-    RoomMode mode = json["mode"];
+    RoomMode mode = (RoomMode)json["roomMode"];
     switch(arena.getAction(json["action"]))
     {
         case GET_ROOMLIST:{
             U32vec idList;
             std::vector<std::string> nameList;
             arena.getRoomList(mode, idList, nameList);
+            data["idList"] = idList;
+            data["nameList"] = nameList;
         }break;
         case GET_ROOMINFO:{
             std::string name;
             U32vec player;
             arena.getRoomInfo(mode, json["roomID"], name, player);
+            data["roomID"] = json["roomID"];
+            data["roomName"] = name;
+            data["playerId"] = player;
         }break;
         case CREATE_ROOM:{
-            uint32_t roomid = arena.createRoom(json["playerID"], mode, json["name"], json["password"]);
+            uint32_t roomid = arena.createRoom(json["userId"], mode, json["name"], json["password"]);
+            data["userId"] = json["userId"];
+            data["roomId"] = roomid==-1?0:roomid;
+            data["roomName"] = json["name"];
         }break;
         case ENTER_ROOM:{
-            bool success = arena.enterRoom(json["playerID"], mode, json["name"], json["password"]);
+            bool success = arena.enterRoom(json["userId"], mode, json["name"], json["password"]);
+            data["result"] = (int)success;
         }break;
         case ENTER_ROOM_RANDOM:{
-            uint32_t roomid = arena.enterRoomRandom(json["playerID"], mode);
+            uint32_t roomid = arena.enterRoomRandom(json["userId"], mode);
+            data["userId"] = json["userId"];
+            data["roomId"] = roomid;
         }break;
         case INVITE_FRIEND:{
-            bool success = arena.inviteFriend(json["playerID"], mode, json["roomID"]);
+            bool success = arena.inviteFriend(json["userId"], mode, json["roomID"]);
+            data["result"] = (int)success;
         }break;
         case START_GAME:{
             arena.startGame(mode, json["roomID"]);
         }break;
         default:
-            result = arena.controlDesk(mode, json["roomID"], json);
+            data = arena.controlDesk(mode, json["roomID"], json);
             break;
     }
+    data["eventType"] = "room";
+    data["roomMode"] = json["roomMode"];
+    data["action"] = json["action"];
+    result["data"] = data;
     return result;
 }
