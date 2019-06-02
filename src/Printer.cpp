9a5dea9cdff19e7266b71b9b02633771b392d235
Jacky Ho <jackyhobingo@gmail.com>
2019-06-02 16:57:03 +0800

    Add time data when print

--- src/Printer.cpp
+++ src/Printer.cpp
@@ -4,8 +4,10 @@
 
 #include "Printer.h"
 #include <nlohmann/json.hpp>
+#include "time.h"
 #include <iostream>
 
 void Printer::print(nlohmann::json &j) {
+    j["time"] = time(nullptr);
     std::cout << j << std::endl;
 }
